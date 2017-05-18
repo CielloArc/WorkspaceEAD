b84e24d9e2fc1e9a0423858fc075544748a17408
logonrm <logonrm@fiap.com.br>
2017-05-18 09:15:36 -0300

    Aula antes do intervalo

---
+++ 15-EJB/src/br/com/fiap/bean/FiapBean.java
@@ -0,0 +1,52 @@
+package br.com.fiap.bean;
+
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+
+import br.com.fiap.bo.FiapBO;
+
+@ManagedBean
+public class FiapBean {
+	private double am;
+	private double nac;
+	private double enade;
+	private double ps;
+	
+	@EJB
+	private FiapBO bo;
+	
+	
+	public void calcular(){
+		ps = bo.calcularPS(nac, am, enade);
+	}
+	
+	public void calcularEnade()	{
+		enade = bo.calcularEnade(nac, am, ps);
+	}
+		
+	public double getAm() {
+		return am;
+	}
+	public void setAm(double am) {
+		this.am = am;
+	}
+	public double getNac() {
+		return nac;
+	}
+	public void setNac(double nac) {
+		this.nac = nac;
+	}
+	public double getEnade() {
+		return enade;
+	}
+	public void setEnade(double enade) {
+		this.enade = enade;
+	}
+	public double getPs() {
+		return ps;
+	}
+	public void setPs(double ps) {
+		this.ps = ps;
+	}
+	
+}
---
+++ 15-EJB/src/br/com/fiap/bo/FiapBO.java
@@ -0,0 +1,6 @@
+package br.com.fiap.bo;
+
+public interface FiapBO {
+	public double calcularPS(double nac, double am, double enade);
+	public double calcularEnade(double nac, double am, double ps);
+}
