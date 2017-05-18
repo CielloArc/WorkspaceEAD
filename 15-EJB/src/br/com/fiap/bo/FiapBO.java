7cedc6c49bad6f11ecd6789db1a0bab100083532
logonrm <logonrm@fiap.com.br>
2017-05-18 09:22:33 -0300

    Antes do intervalo

--- 15-EJB/src/br/com/fiap/bo/FiapBO.java
+++ 15-EJB/src/br/com/fiap/bo/FiapBO.java
@@ -1,5 +1,8 @@
 package br.com.fiap.bo;
 
+import javax.ejb.Remote;
+
+@Remote
 public interface FiapBO {
 	public double calcularPS(double nac, double am, double enade);
 	public double calcularEnade(double nac, double am, double ps);
